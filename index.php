2373866679d486ecb4eee076581bba8f1e47235c
Eko Wijaya <ekowijayawindows@gmail.com>
2021-04-27 11:04:24 +0700

    PHP

---
+++ index.php
@@ -0,0 +1,26 @@
+<html>
+<head>
+	<title>Membuat form validasi</title>
+</head>
+<body>
+	<h1>Membuat Form Validasi Dengan PHP</h1>
+	<?php
+	if(isset($_GET['nama'])){
+		if($_GET['nama'] == "kosong"){
+			echo "<h4 style='color:red'>Nama Belum di Masukkan !</h4>";
+
+		}
+	}
+	?>
+	<h4> Masukkan nama anda : </h4>
+	<form action="hasil_form.php" method="post">
+		<table>
+			<tr>
+				<td>Nama</td>
+				<td><input type="text" name="nama"></td>
+				<td><input type="submit" value="cek"></td>
+			</tr>
+		</table>
+	</form>
+</body>
+</html>
