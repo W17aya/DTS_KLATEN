b579272640e32fc3ce4b4fef8e5e3e87cccf5bcf
Eko Wijaya <ekowijayawindows@gmail.com>
2021-04-27 08:14:37 +0700

    Tugas 1 Membuat Login Page

---
+++ validasi.js
@@ -0,0 +1,22 @@
+function cekform() {
+    //buat variabel untuk menampung inputan
+    nama = document.getElementById('txtnama');
+    olahraga = documen.getElementById('olahraga');
+    if (nama.value == '') {
+        alert('nama tidak boleh kosong');
+        nama.focus();
+        return false;
+    } else if (nama.value.length <= 3) {
+        alert('nama minimal 3 karakter');
+
+        nama.focus();
+        return false;
+    } else if (olahraga.value == '') {
+        alert('olahraga belum dipilih');
+        olahraga.focus();
+        return false;
+    } else {
+        alert('terima kasih telah mengisi form');
+        return true;
+    }
+}
